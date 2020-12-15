580d15c079e2cdb086069e1ac1bbbca21f0f03bb
Wisp <52381993+Wisp42@users.noreply.github.com>
2020-12-16 01:04:41 +0200

    Stud+ 1.0.1 (Surgery 70%)

--- scripts/focus.js
+++ scripts/focus.js
@@ -1,30 +1,30 @@
-var focus_items = document.querySelectorAll('.focus');
-var focus_title_items = document.querySelectorAll('.focus-title');
-var focus_content_items = document.querySelectorAll('.focus-content');
-var test1= document.getElementById('test1');
-// console.log(focus_content_items[0].className)
-
-for(var f = 0; f < focus_items.length; f++){
-	focus_items[f].onclick = function(){
-		if (focus_content_items[f].className == 'focus-content' || focus_content_items[f].className == 'focus-content focus-content_enable'){
-			// console.log(focus_content_items[0].className);
-			focus_content_items[f].className = 'focus-content focus-content_disable';
-		}else if (focus_content_items[f].className == 'focus-content focus-content_disable'){
-			focus_content_items[f].className == 'focus-content focus-content_ensable'
-		}
-	};
-}
+// var focus_items = document.querySelectorAll('.focus');
+// var focus_title_items = document.querySelectorAll('.focus-title');
+// var focus_content_items = document.querySelectorAll('.focus-content');
+// var test1= document.getElementById('test1');
+// // console.log(focus_content_items[0].className)
 
 // for(var f = 0; f < focus_items.length; f++){
-// 	function focus_toggle() {
-// 		if (focus_content_items[f].className == 'focus-content' || focus_content_items[f].className == 'focus-content focus-content_disable'){
-// 			console.log(focus_content_items[f].className)
-// 			// focus_content_items[f].className = 'focus-content focus-content_disable';
-// 		}else if (focus_content_items[f].className == 'focus-content focus-content_enable'){
-// 			// focus_content_items[f].className == 'focus-content focus-content_disable'
+// 	focus_items[f].onclick = function(){
+// 		if (focus_content_items[f].className == 'focus-content' || focus_content_items[f].className == 'focus-content focus-content_enable'){
+// 			// console.log(focus_content_items[0].className);
+// 			focus_content_items[f].className = 'focus-content focus-content_disable';
+// 		}else if (focus_content_items[f].className == 'focus-content focus-content_disable'){
+// 			focus_content_items[f].className == 'focus-content focus-content_ensable'
 // 		}
-// 	}
-// 	focus_items[f].addEventListener('click', focus_toggle)
+// 	};
 // }
-// console.log(focus_items[0].className)
-// console.log(focus_content_items[0].className)
+
+// // for(var f = 0; f < focus_items.length; f++){
+// // 	function focus_toggle() {
+// // 		if (focus_content_items[f].className == 'focus-content' || focus_content_items[f].className == 'focus-content focus-content_disable'){
+// // 			console.log(focus_content_items[f].className)
+// // 			// focus_content_items[f].className = 'focus-content focus-content_disable';
+// // 		}else if (focus_content_items[f].className == 'focus-content focus-content_enable'){
+// // 			// focus_content_items[f].className == 'focus-content focus-content_disable'
+// // 		}
+// // 	}
+// // 	focus_items[f].addEventListener('click', focus_toggle)
+// // }
+// // console.log(focus_items[0].className)
+// // console.log(focus_content_items[0].className)
